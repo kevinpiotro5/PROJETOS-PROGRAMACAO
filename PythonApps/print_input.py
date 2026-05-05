d3d62aa05a490acbc9c545918b89a40ca1964a4c
Kevin Piotrowski <kevinpiotrowski@192.168.0.5>
2026-05-05 16:57:57 -0300

    first commit

---
+++ PythonApps/print_input.py
@@ -0,0 +1,9 @@
+nome = input("Informe o seu nome:")
+idade = input("Informe a sua idade:")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
+print(nome, idade,end="... \n", sep="#")
+
+print(5 // 2)
---
+++ PythonApps/variaveis_constantes.py
@@ -0,0 +1,11 @@
+nome = "Kevin"
+idade = 25
+
+nome= "Giovanna"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP"]
+print(BRAZILIAN_STATES)
